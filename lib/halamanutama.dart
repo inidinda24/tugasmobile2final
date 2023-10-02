fc8238b66bf537bb680ff3d857b019cb3906a2a6
inidinda24 <145177254+inidinda24@users.noreply.github.com>
2023-10-02 14:54:26 +0700

    menambahkan elevated button

--- lib/halamanutama.dart
+++ lib/halamanutama.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_mobile2/halamanlogin.dart';
 
 class halamanutama extends StatelessWidget {
   const halamanutama({super.key});
@@ -9,6 +10,22 @@ class halamanutama extends StatelessWidget {
       appBar: AppBar(
         title: Text("My Stopwatch", style: TextStyle(color: Colors.white),),
         backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Ini Halaman 1"),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return halamanlogin();
+                  }));
+                },
+                child: Text("Ke Page 2")),
+            
+          ],
+        ),
       ),
     );
   }
